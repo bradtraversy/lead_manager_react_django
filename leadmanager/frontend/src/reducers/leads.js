ad30466a2b00aded5d4367adff3dd7f84f0c568c
dukarc <cyrus.dukart@gmail.com>
2019-02-23 20:55:44 -0800

    leads.js missing CLEAR_LEADS action
    noticed this was missing when I attempted to do a fresh install

--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,4 +1,4 @@
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "../actions/types.js";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, CLEAR_LEADS } from "../actions/types.js";
 
 const initialState = {
   leads: []
