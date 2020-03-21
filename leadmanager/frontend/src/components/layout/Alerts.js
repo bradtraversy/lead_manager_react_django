d145cc4f758a7c0bf686b36408bbda27a3e8be06
Iurie Tarlev <iura.tarlev@gmail.com>
2020-03-21 00:19:09 +0000

    changing the the call to withAlert(), since react-alerts api changed

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,7 +1,7 @@
-import React, { Component, Fragment } from 'react';
-import { withAlert } from 'react-alert';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Component, Fragment } from "react";
+import { withAlert } from "react-alert";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 export class Alerts extends Component {
   static propTypes = {
