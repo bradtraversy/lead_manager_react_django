a9dfe09cc773e002cca5efd5221d00ca7a0dd4f4
mbedard <m.burrbedard@gmail.com>
2020-04-05 04:45:40 -0400

    Restore prettier path

--- leadmanager/frontend/src/components/common/PrivateRoute.js
+++ leadmanager/frontend/src/components/common/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const PrivateRoute = ({ component: Component, auth, ...rest }) => (
