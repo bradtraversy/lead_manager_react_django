2e95e192f374260e800c328c73037395db40b7a1
mbedard <m.burrbedard@gmail.com>
2020-04-05 03:32:58 -0400

    Fix Object of type AuthToken is not JSON serializable error in accounts/api.py

--- leadmanager/accounts/api.py
+++ leadmanager/accounts/api.py
@@ -24,9 +24,10 @@ class LoginAPI(generics.GenericAPIView):
     serializer = self.get_serializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     user = serializer.validated_data
+    _, token = AuthToken.objects.create(user)
     return Response({
       "user": UserSerializer(user, context=self.get_serializer_context()).data,
-      "token": AuthToken.objects.create(user)
+      "token": token
     })
 
 # Get User API
