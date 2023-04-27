96d3392784088c17a6f35ed25e308d649aaa59a2
Sandesh Thapa <63294026+Sandesh-Thapa@users.noreply.github.com>
2023-04-27 08:50:32 +0545

    Update .babelrc to skip importing react on every component, Added entry and output configuration in webpack.config.js, use webpack.config.js config in package.json scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+var path = require("path")
+
 module.exports = {
+  entry: path.join(__dirname, "leadmanager/frontend/src/index"),
+  output: {
+    path: path.join(__dirname, "leadmanager/frontend/static/frontend"),
+    filename: "[name].js",
+  },
   module: {
     rules: [
       {
